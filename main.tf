6313adf2daccfcc244dbac90889ea623c8807414
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-04-22 19:12:19 +0100

    3rd commit

--- main.tf
+++ main.tf
@@ -3,71 +3,93 @@ provider "aws" {
 }
 
 resource "aws_security_group" "miniproject-sg" {
-  name = "miniproject-sg"
-  description = "Allow neccessary ports"
+  name        = "miniproject-sg"
+  description = "Allow web and SSH traffic"
 
-  ingress = {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks =["0.0.0.0/0"] # restrict to production
-  }
+  ingress = [
+    {
+      description      = "HTTP access"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    },
+    {
+      description      = "SSH access"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
 
-  ingress = {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks =["0.0.0.0/0"]
-  }
+  egress = [
+    {
+      description      = "Allow all outbound"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
 
-  egress = {
-     from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks =["0.0.0.0/0"]
+  tags = {
+    Name = "miniproject-sg"
   }
 }
 
 resource "aws_instance" "terraform-mini-project" {
-  count = 3
-  ami             = var.ami_id
-  instance_type   = var.instance_type
+  count         = 3
+  ami           = var.ami_id
+  instance_type = var.instance_type
   key_name      = var.key_name
-  subnet_id = var.subnet_ID
-  security_groups = [ var.security_group_ID ]
+  subnet_id     = var.subnet_ID
+  #security_groups = [ var.security_group_ID ]
 
   tags = {
     Name = "MyEC2Instance-${count.index}"
   }
-# output the public ips into a file
+  # output the public ips into a file
   provisioner "local-exec" {
-  command = "echo ${self.public_ip} >> host-inventory"
-}
+    command = "echo ${self.public_ip} >> host-inventory"
+  }
 }
 
 resource "aws_lb" "miniproject_lb" {
-  name = "mini-project-load-balancer"
-  internal = false
+  name               = "mini-project-load-balancer"
+  internal           = false
   load_balancer_type = "application"
-  security_groups = [aws_security_group.miniproject-sg]
-  subnets = var.subnet_ID
+  security_groups    = [aws_security_group.miniproject-sg.id]
+  subnets            = [var.subnet_ID]
 
   enable_deletion_protection = false
 }
 
 resource "aws_lb_target_group" "miniproject_tg" {
-  name = "miniproject-tg"
-  port = 80
+  name     = "miniproject-tg"
+  port     = 80
   protocol = "HTTP"
-  vpc_id = var.vpc_ID
+  vpc_id   = var.vpc_ID
 }
 resource "aws_lb_listener" "miniproject_listener" {
   load_balancer_arn = aws_lb.miniproject_lb.arn
-  port = 80
+  port              = 80
   default_action {
     type = "fixed-response"
     fixed_response {
-      status_code = 200
+      status_code  = 200
       content_type = "text/plain"
       message_body = "Helo from the other side!"
     }
@@ -75,8 +97,8 @@ resource "aws_lb_listener" "miniproject_listener" {
 }
 
 resource "aws_lb_target_group_attachment" "miniproject_attachment" {
-  count = 3
+  count            = 3
   target_group_arn = aws_lb_target_group.miniproject_tg.arn
-  target_id = aws_instance.terraform-mini-project[count.index].ID
-  port = 80
+  target_id        = aws_instance.terraform-mini-project[count.index].id
+  port             = 80
 }
--- variable.tf
+++ variable.tf
@@ -1,24 +1,24 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "eu-north-1"
+  default     = "eu-north-1"
 }
 
 variable "instance_type" {
   description = "Instance type"
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "ami_id" {
   description = "Amazon Machine Image (AMI) ID"
 }
 
-variable "key_name"{
+variable "key_name" {
   description = "Name of EC2 key pair"
 }
 
-variable "security_group_ID" {
-  description = "Security Group ID"
-}
+# variable "security_group_ID" {
+# description = "Security Group ID"
+#}
 
 variable "subnet_ID" {
   description = "Subnet ID"
